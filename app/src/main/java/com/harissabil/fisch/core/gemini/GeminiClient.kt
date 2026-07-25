cb618afa4ebd7cedc59aa0d78795e57c77b44bc1
Muhammad Haris Sabil Al Karim <harissabil04@gmail.com>
2026-07-26 06:34:15 +0700

    Update Gemini model name to GEMINI_FLASH

--- app/src/main/java/com/harissabil/fisch/core/gemini/GeminiClient.kt
+++ app/src/main/java/com/harissabil/fisch/core/gemini/GeminiClient.kt
@@ -2,13 +2,13 @@ package com.harissabil.fisch.core.gemini
 
 import com.google.ai.client.generativeai.GenerativeModel
 import com.harissabil.fisch.BuildConfig
-import com.harissabil.fisch.core.common.util.Constant.GEMINI_PRO_VISION
+import com.harissabil.fisch.core.common.util.Constant.GEMINI_FLASH
 
 class GeminiClient {
 
     val geneminiProVisionModel by lazy {
         GenerativeModel(
-            modelName = GEMINI_PRO_VISION,
+            modelName = GEMINI_FLASH,
             apiKey = BuildConfig.GEMINI_API_KEY,
         )
     }
